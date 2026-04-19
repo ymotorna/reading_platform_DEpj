e2e2eed1911802d20ecb590de379a257c0ebcb54
ymotorna <155849194+ymotorna@users.noreply.github.com>
2026-04-19 17:20:36 +0300

    work on dag

--- dbt/dbt_reading_platform/check_analyse.sql
+++ dbt/dbt_reading_platform/check_analyse.sql
@@ -1,3 +1,4 @@
+
 -- check
 SELECT count(*) FROM dim_users;
 SELECT count(*) FROM dim_authors;
@@ -12,7 +13,7 @@ SELECT count(*) FROM fct_transactions;
 
 -- user engagement
 select user_id,
-       country,
+       country_code,
        total_pages_read,
        books_completed,
        round(lifetime_payments, 2) as lifetime_oayments,
@@ -23,22 +24,24 @@ order by revenue_rank asc
 limit 10;
 
 -- genre perfomance
-select genre,
-       count(book_id) as num_books,
-       count(distinct author_id) as num_authors,
-       sum(total_readers) as total_readers,
-       sum(total_Sessions) as total_sessions,
-       avg(genre_avg_rating) as genre_avg_rateing
-from book_performance
+select g.genre_name,
+       count(bp.book_id) as num_books,
+       count(distinct bp.author_id) as num_authors,
+       sum(bp.total_readers) as total_readers,
+       sum(bp.total_Sessions) as total_sessions,
+       round(avg(bp.genre_avg_rating), 2) as genre_avg_rateing
+from genres g
+left join book_performance bp
+    on g.genre_id = bp.genre_id
 where is_available=true
-group by genre
+group by genre_name
 order by genre_avg_rateing desc;
 
 
 -- revenue
 select date_trunc('month', billing_period_start) as month,
     plan_type,
-    sum(payment_amount) as total_revenue,
+    round(sum(payment_amount), 2) as total_revenue,
     round(sum(payment_amount) / sum(sum(payment_amount)) over () * 100, 2) as pct_of_total_revenue,
     mode() within group (order by payment_method) as top_payment_method
 from fct_transactions
